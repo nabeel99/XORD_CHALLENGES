db8309ca4939dd657d095f421285aaa3add68d64
Nabeel <ac3r_nabeel@live.com>
2021-08-14 10:29:58 +0500

    sol

--- erc1155.sol
+++ erc1155.sol
@@ -1,4 +1,3 @@
-
 pragma solidity^0.8.6;
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
@@ -46,9 +45,10 @@ contract task4 is IERC1155{
     }
     function setApprovalForAll(address operator, bool approved)
     public override {
-        
-        authorizedAll[msg.sender][operator] = approved;
-         emit ApprovalForAll(msg.sender, operator, approved);
+        address send = msg.sender;
+        require(send!=operator);
+        authorizedAll[send][operator] = approved;
+         emit ApprovalForAll(send, operator, approved);
     }
     function isApprovedForAll(address account, address operator) 
     public override view
@@ -87,17 +87,25 @@ contract task4 is IERC1155{
     public override {
          require(amounts.length==ids.length,
         "size mismatch between id and accounts");
+        require(from==msg.sender|| isApprovedForAll(from,msg.sender));
+        require(to!=address(0));
+         
+        for(uint i=0; i<amounts.length;i++){
+            
+           uint256 id = ids[i];
+            uint256 amount = amounts[i];
+            uint256 fromBalance = _balance[id][from];
+            require(fromBalance >= amount, "ERC1155: insufficient balance for transfer");
+            _balance[id][from] = fromBalance -amount;
+            _balance[id][to] +=amount;
+        }
+       
+        emit TransferBatch(msg.sender, from, to, ids, amounts);
          if(isContract(to)) {
             IERC1155Receiver receiver = IERC1155Receiver(to);
             require(receiver.onERC1155BatchReceived(msg.sender, from, ids, amounts, data)==
             bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)")));
-            
-    
         }
-        for(uint i=0; i<amounts.length;i++){
-            safeTransferFrom(from,to,ids[i],amounts[i]);
-        }
-        emit TransferBatch(msg.sender, from, to, ids, amounts);
     }
     function isContract(address _addr) 
        private view
